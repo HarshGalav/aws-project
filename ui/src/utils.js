c8affb6d10fb2a591c0a2a9f3946f6e57dc1cc92
HarshGalav <harshsharma3122@gmail.com>
2024-11-22 13:02:45 +0530

    changes

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://hhybumf73lfqse3akm4wcyorta0zooib.lambda-url.eu-north-1.on.aws/task ";
+export const API_URL = "https://3c22hvy7l74vtnf5uzrcnyaxpq0hfwff.lambda-url.eu-north-1.on.aws/task ";
